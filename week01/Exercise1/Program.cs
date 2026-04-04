f7f5cf06566a722becca024e3bae7fe2c445fed2
FaithMak13 <mak20003@byui.edu>
2026-04-04 08:49:09 +0200

    Finished exercise 1

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,13 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("What is your first name? ");
-string firstName = Console.ReadLine();
+        Console.Write("What is your first name?");
+        string first = Console.ReadLine();
 
-Console.Write("What is your last name? ");
-string lastName = Console.ReadLine();
+        Console.Write("What is your last name?");
+        string last = Console.ReadLine();
 
-Console.WriteLine();
-Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
-    }
-}
+        Console.WriteLine($"Your name is {last}, {first} {last}.");
+
