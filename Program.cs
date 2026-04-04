cf344cef2a876814fb59fef342ab15c2b5e08d4c
FaithMak13 <mak20003@byui.edu>
2026-04-04 09:57:56 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("What is your first name?");
+        string first = Console.ReadLine();
+
+        Console.Write("What is your last name?");
+        string last = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {last}, {first} {last}.");
+
+<<<<<<< HEAD
+=======
+
+    
+    }
+}
+>>>>>>> 0b0d8f2 (Add VERSIONCONTROLANSWERS, Exercise 1 and 2 solutions)
