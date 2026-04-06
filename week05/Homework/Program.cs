0245fbacec88d005239ca597db23571813c3e415
Faith <fmakawure@byupathway.edu>
2026-04-06 13:35:52 +0200

    W05 Learning Activity: Inheritance

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -1,9 +1,26 @@
-using System;
+namespace Homework;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+      //Test base Assignment
+    Assignment assignment = new Assignment("Samuel Bennet", "Multiplication");
+    Console.WriteLine(assignment.GetSummary());
+
+    Console.WriteLine();
+    //Test MathAssignment
+    MathAssignment mathAssignment = new MathAssignment("Roberto Rodriguez", "Fractions", "Section 7.3", "1-10");
+    Console.WriteLine(mathAssignment.GetSummary());
+    Console.WriteLine(mathAssignment.GetHomeworkList());            
+
+    Console.WriteLine();
+
+    //Test WritingAssignment
+    WritingAssignment writingAssignment = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+    Console.WriteLine(writingAssignment.GetSummary());
+    Console.WriteLine(writingAssignment.GetWritingInformation());   
+
     }
 }
+
